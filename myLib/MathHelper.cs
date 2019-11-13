a17e44be3cf8545e57070e0012aecf39c2e2b273
hmandiv321 <hmandiv@outlook.com>
2019-11-12 19:22:23 -0500

    Add test fot commutative property

--- myLib/MathHelper.cs
+++ myLib/MathHelper.cs
@@ -4,6 +4,20 @@ namespace myLib
 {
     public class MathHelper
     {
+        /// <summary>
+        /// Accepts two nunbers and checks if the two numbers are same,
+        /// if the numbers are same then it returns the number, ex: Max(0,0) 
+        /// returns 0.
+        /// if one number is greater than the other, then the greater number is 
+        /// returned, ex: Max(89,99), returns 99.
+        /// The Max method is commutative - returns the same result independent 
+        /// of the order of the operands.
+        /// </summary>
+        /// <param name="numberOne"></param>
+        /// <param name="numberTwo"></param>
+        /// <returns>
+        /// Max number between the two numbers entered as input
+        /// </returns>
         public int Max(int numberOne, int numberTwo)
         {
             /*since this is TDD, now that our implementation of the test fails
@@ -13,6 +27,16 @@ namespace myLib
             {
                 return numberOne;
             }
+            
+            if(numberOne > numberTwo)
+            {
+                return numberOne;
+            }
+            else
+            {
+                return numberTwo;
+            }
+
             throw new NotImplementedException("Write tests!");
         }
 
--- myTests/MathHelper_MaxShouldDo.cs
+++ myTests/MathHelper_MaxShouldDo.cs
@@ -15,6 +15,10 @@ namespace myTests
         }
 
         [Theory]
+        /// <summary>
+        /// Tests if two numbers enered are same then the   
+        /// number returned is same as the numbers entered.
+        /// </summary>
         [InlineData(89,89,89)]
         [InlineData(-999, -999, -999)]
         [InlineData(270, 270, 270)]
@@ -26,6 +30,45 @@ namespace myTests
 
             //Assert
             Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        /// <summary>
+        /// Tests if the max value of the two given number   
+        /// is returned.
+        /// </summary>
+        [InlineData(89, 88, 89)]
+        [InlineData(-999, 999, 999)]
+        [InlineData(299, 270, 299)]
+        [InlineData(3, 1002, 1002)]
+        [InlineData(-3453, -1002, -1002)]
+        public void ReturnMaxValueOfTheTwoGivenNumbers(int value1, int value2, int expectedResult)
+        {
+            //Act
+            var result = _mathHelper.Max(value1, value2);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        /// <summary>
+        /// Test the fact that Max is commutative - returns the same 
+        /// result independent of the order of the operands.
+        /// </summary>
+        public void ReturnSameResultIndependentOfOrder()
+        {
+            //Arrange
+            var one = 19;
+            var two = 89;
+
+            //Act
+            var result1 = _mathHelper.Max(one, two);
+            var result2 = _mathHelper.Max(two,one);
+
+            //Assert
+            Assert.Equal(result1, result2);
+
         }
     }
 }
